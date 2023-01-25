c1e77b6e087d4964f5b9cf03780f73ca0e6c4bcd
Shlgnaa <shlgnaa@gmail.com>
2023-01-25 17:28:32 +0300

    add brain-even

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
+import { welcomeMessage } from '../src/cli.js';
 
-import { nameLogin } from '../src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-
-nameLogin();
+welcomeMessage();
